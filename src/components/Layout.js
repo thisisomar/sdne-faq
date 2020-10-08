58a08d5c67c87606d6e4f0a3fbd26a76f2a2c03d
thisisomar <omarcontactme@gmail.com>
2020-10-08 01:05:19 -0400

    Inconsistencies are now consistent

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,10 @@ import React from 'react';
 import {
   ThemeProvider,
   CssBaseline
-} from "@material-ui/core";
+} from '@material-ui/core';
 import { dark } from '../theme';
 
-import Header from "../components/Header";
+import Header from '../components/Header';
 
 const Layout = ({ children }) => (
   <>
