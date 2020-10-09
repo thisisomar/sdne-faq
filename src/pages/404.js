83d6a07c8cb169ff2a31f93273b704ccee72a868
scuffedfox <kkroberts1635@gmail.com>
2020-10-09 19:53:01 -0400

    Added media queries to header.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function NotFound() {
   if (typeof window !== 'undefined') {
     window.location = '/';
--- src/theme.js
+++ src/theme.js
@@ -5,11 +5,24 @@ const darkMuiTheme = createMuiTheme({
   type: 'dark',
   palette: {
     primary: {
+      dark: '#16131d',
       main: '#1f1b29'
     },
     background: {
       default: '#1f1b29'
-    }
+    },
+    typography: {
+
+      primary: {
+        
+        fontFamily: [
+            'Poppins',
+            'Roboto',
+            'sans-serif'
+          ].join(','),
+        },
+  
+      },
   }
 })
 
