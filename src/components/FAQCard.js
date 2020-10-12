e332dd71ab1266718327b5d5174321ae04209559
Jacob Brasil <jnxtbrasil@gmail.com>
2020-10-11 21:28:42 -0400

    Added Share button to Copy to Clipboard

--- src/components/FAQCard.js
+++ src/components/FAQCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 import {
   Card,
   CardContent,
@@ -7,120 +7,120 @@ import {
   makeStyles,
   createStyles,
   Button,
-  Box
-} from '@material-ui/core';
+  Box,
+} from "@material-ui/core"
 
-import { Link } from 'gatsby'
-import LinkIcon from '@material-ui/icons/Link';
+import { Link } from "gatsby"
+import LinkIcon from "@material-ui/icons/Link"
 
+import { copyToClipboard } from "../utils/CopyToClipboard"
 
-const useStyles = makeStyles((theme) => createStyles ({
-  title: {
-    marginBottom: 7,
-    fontWeight: 700,
-    fontFamily: 'Poppins, sans-serif',
-    color: "#ffffff",
+const useStyles = makeStyles(theme =>
+  createStyles({
+    title: {
+      marginBottom: 7,
+      fontWeight: 700,
+      fontFamily: "Poppins, sans-serif",
+      color: "#ffffff",
 
-    [theme.breakpoints.down('xl')]: {
-      fontSize: '3em'
-    },
-    [theme.breakpoints.down('lg')]: {
-      fontSize: '2.5em'
-    },
-    [theme.breakpoints.down('md')]: {
-        fontSize: '2.2em',
-    },
-    [theme.breakpoints.down('sm')]: {
-        fontSize: '1.9em'
-    },
-    [theme.breakpoints.down('xs')]: {
-        fontSize: '1.6em',
-    }
-  },
-  size: {
-    width: "100%",
-    backgroundColor: theme.palette.primary.dark,
-    
-    [theme.breakpoints.down('xl')]: {
-      maxWidth: 1200,
-      padding: "25px 35px"
-    },
-    [theme.breakpoints.down('lg')]: {
-      
-    },
-    [theme.breakpoints.down('md')]: {
-      
-    },
-    [theme.breakpoints.down('sm')]: {
-        
-    },
-    [theme.breakpoints.down('xs')]: {
-      maxWidth: "100%",
-      padding: "20px"
-    }
-  },
-  body: {
-    lineHeight: 1.75,
-    color: "#ffffff",
-    fontFamily: 'Roboto, sans-serif',
-    fontWeight: 300,
-
-    '& p, li': {
-      [theme.breakpoints.down('xl')]: {
-        margin: "10px 0px 0px 0px",
-        fontSize: '1.5em'
+      [theme.breakpoints.down("xl")]: {
+        fontSize: "3em",
       },
-      [theme.breakpoints.down('lg')]: {
-        ontSize: '1.4em'
+      [theme.breakpoints.down("lg")]: {
+        fontSize: "2.5em",
       },
-      [theme.breakpoints.down('md')]: {
-        ontSize: '1.25em'
+      [theme.breakpoints.down("md")]: {
+        fontSize: "2.2em",
       },
-      [theme.breakpoints.down('sm')]: {
-        ontSize: '1.15em'
+      [theme.breakpoints.down("sm")]: {
+        fontSize: "1.9em",
       },
-      [theme.breakpoints.down('xs')]: {
-        fontSize: '1.1em'
-      }
+      [theme.breakpoints.down("xs")]: {
+        fontSize: "1.6em",
+      },
     },
+    size: {
+      width: "100%",
+      backgroundColor: theme.palette.primary.dark,
 
-    '& ul': {
-      paddingLeft: 22,
-      margin: 0
+      [theme.breakpoints.down("xl")]: {
+        maxWidth: 1200,
+        padding: "25px 35px",
+      },
+      [theme.breakpoints.down("lg")]: {},
+      [theme.breakpoints.down("md")]: {},
+      [theme.breakpoints.down("sm")]: {},
+      [theme.breakpoints.down("xs")]: {
+        maxWidth: "100%",
+        padding: "20px",
+      },
     },
+    body: {
+      lineHeight: 1.75,
+      color: "#ffffff",
+      fontFamily: "Roboto, sans-serif",
+      fontWeight: 300,
 
-    '& a': {
-      color: theme.palette.secondary.main,
-      textDecoration: "none"
-    }
+      "& p, li": {
+        [theme.breakpoints.down("xl")]: {
+          margin: "10px 0px 0px 0px",
+          fontSize: "1.5em",
+        },
+        [theme.breakpoints.down("lg")]: {
+          ontSize: "1.4em",
+        },
+        [theme.breakpoints.down("md")]: {
+          ontSize: "1.25em",
+        },
+        [theme.breakpoints.down("sm")]: {
+          ontSize: "1.15em",
+        },
+        [theme.breakpoints.down("xs")]: {
+          fontSize: "1.1em",
+        },
+      },
 
-  },
-  controlBox: {
-    marginTop: 15
-  },
-  cardContent: {
-    padding: 0,
+      "& ul": {
+        paddingLeft: 22,
+        margin: 0,
+      },
 
-    '&:last-child': {
-      padding: 0
+      "& a": {
+        color: theme.palette.secondary.main,
+        textDecoration: "none",
+      },
     },
-  }
-}));
+    controlBox: {
+      marginTop: 15,
+    },
+    cardContent: {
+      padding: 0,
 
-export default function({ faq }) {
-  const classes = useStyles();
+      "&:last-child": {
+        padding: 0,
+      },
+    },
+  })
+)
 
-  return(
+export default function ({ faq }) {
+  const classes = useStyles()
+
+  return (
     <Card className={classes.size}>
-      <CardContent className={classes.cardContent}> 
-
+      <CardContent className={classes.cardContent}>
         <Typography className={classes.title} variant="h3">
-          { faq.frontmatter.question }
+          {faq.frontmatter.question}
         </Typography>
 
-        <Divider/>
-        
-        <Typography className={classes.body} color="textPrimary" component="div" dangerouslySetInnerHTML={{ __html: faq.html }} />
+        <Divider />
+
+        <Typography
+          className={classes.body}
+          color="textPrimary"
+          component="div"
+          dangerouslySetInnerHTML={{ __html: faq.html }}
+        />
 
         <Box className={classes.controlBox}>
           <Button
@@ -128,13 +128,15 @@ export default function({ faq }) {
             color="secondary"
             component={Link}
             className={classes.button}
-            to={faq.frontmatter.slug}
+            to='.'
+            onClick={() => {
+              copyToClipboard(window.location.href + (window.location.href.indexOf(faq.frontmatter.slug) == -1 ? faq.frontmatter.slug : ''))
+            }}
             startIcon={<LinkIcon />}
           >
             Share
           </Button>
         </Box>
-
       </CardContent>
     </Card>
   )
