cf9c56483b27dd6e186512e6d6c0a76bdecaea51
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-19 04:11:55 -0500

    Added dummy Search Bar that needs to be styled

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,33 +1,29 @@
-import React from 'react';
-import {
-  ThemeProvider,
-  CssBaseline,
-  Box,
-  Grid
-} from '@material-ui/core';
-import { dark } from '../theme';
+import React from "react"
+import { ThemeProvider, CssBaseline, Box, Grid } from "@material-ui/core"
+import { dark } from "../theme"
 
-
-import Header from '../components/Header';
+import Header from "../components/Header"
+import SearchBar from "../components/SearchBar"
 
 const Layout = ({ children }) => (
   <>
     <ThemeProvider theme={dark}>
-      <CssBaseline/>
-      <Header/>
-      <Box pl={3} pr={3} pt={5} pb={5} maxWidth='sm'>
+      <CssBaseline />
+      <Header />
+      <Box pl={3} pr={3} pt={5} pb={5} maxWidth="sm">
         <Grid
           container
-          direction='column'
-          justify='center'
-          alignItems='center'
+          direction="column"
+          justify="center"
+          alignItems="center"
           spacing={4}
         >
-          { children }
+          <SearchBar />
+          {children}
         </Grid>
       </Box>
     </ThemeProvider>
   </>
 )
 
-export default Layout;
+export default Layout
---
+++ src/components/SearchBar.js
@@ -0,0 +1,62 @@
+import React from "react"
+import {
+  TextField,
+  Card,
+  CardContent,
+  Typography,
+  makeStyles,
+  createStyles,
+} from "@material-ui/core"
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {},
+    searchBar: {
+      fontFamily: "Poppins, sans-serif",
+      color: "#ffffff",
+    },
+    size: {
+      width: "100%",
+      backgroundColor: theme.palette.primary.dark,
+
+      [theme.breakpoints.down("xl")]: {
+        maxWidth: 1200,
+        padding: "25px 35px",
+      },
+      [theme.breakpoints.down("lg")]: {},
+      [theme.breakpoints.down("md")]: {},
+      [theme.breakpoints.down("sm")]: {},
+      [theme.breakpoints.down("xs")]: {
+        maxWidth: "100%",
+        padding: "20px",
+      },
+    },
+    cardContent: {
+      padding: 0,
+
+      "&:last-child": {
+        padding: 0,
+      },
+    },
+  })
+)
+
+export default function SearchBar() {
+  const classes = useStyles()
+
+  return (
+    <Card className={classes.size}>
+      <CardContent className={classes.cardContent}>
+        <TextField
+          id="standard-search"
+          color="secondary"
+          className={classes.root}
+          InputProps={{ className: classes.searchBar }}
+          fullWidth
+          label="Search field"
+          type="search"
+        />
+      </CardContent>
+    </Card>
+  )
+}
