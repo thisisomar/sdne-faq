5e1fee4321a65ce4e0ff1875f666f02c09c6f15d
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2021-11-14 15:03:12 -0500

    refactor(SearchBar): update to be context independent
    This will make it more modular and reusable in multiple places.
    
    Start prefixing mui components with `MUI` so we can easily recognise them

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,89 +1,22 @@
-import React, { useState } from "react"
-import {
-  TextField,
-  Card,
-  CardContent,
-  makeStyles,
-  createStyles,
-} from "@material-ui/core"
+import React from "react"
+import { TextField as MUITextField } from "@mui/material"
 
-import { Grid } from "@material-ui/core"
-import FAQCard from "../components/FAQCard.js"
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    root: {},
-    searchBar: {
-      fontFamily: "Poppins, sans-serif",
-      color: "#ffffff",
-    },
-    size: {
-      width: "100%",
-      backgroundColor: theme.palette.primary.dark,
-
-      [theme.breakpoints.down("xl")]: {
-        maxWidth: 1200,
-        padding: "25px 35px",
-      },
-      [theme.breakpoints.down("lg")]: {
-      },
-      [theme.breakpoints.down("md")]: {
-        maxWidth: "97%",
-      },
-      [theme.breakpoints.down("sm")]: {
-        maxWidth: "96%",
-      },
-      [theme.breakpoints.down("xs")]: {
-        maxWidth: "94%",
-        padding: "10px",
-      },
-    },
-    cardContent: {
-      padding: 0,
-
-      "&:last-child": {
-        padding: 0,
-      },
-    },
-  })
-)
-
-export default function SearchBar({ faqs }) {
-  const [query, setQuery] = useState("")
-  const classes = useStyles()
-
-  const search = event => {
-    setQuery(event.target.value)
+export default function SearchBar({ onSearchQueryHandler }) {
+  const handleInputChange = event => {
+    onSearchQueryHandler(event.target.value)
   }
 
   return (
     <>
-      <Card className={classes.size}>
-        <CardContent className={classes.cardContent}>
-          <TextField
-            id="standard-search"
-            color="secondary"
-            InputLabelProps={{ className: classes.searchBar }}
-            className={classes.root}
-            InputProps={{ className: classes.searchBar }}
-            fullWidth
-            onChange={search}
-            label="Search SDNE FAQ"
-            type="search"
-            variant="outlined"
-          />
-        </CardContent>
-      </Card>
-      {faqs.map(edge =>
-        edge.node.rawMarkdownBody.toLowerCase().includes(query.toLowerCase()) ||
-        edge.node.frontmatter.question
-          .toLowerCase()
-          .includes(query.toLowerCase()) ? (
-          <Grid key={edge.node.id} item xs={12}>
-            <FAQCard key={edge.node.id} faq={edge.node} />
-          </Grid>
-        ) : null
-      )}
+      <MUITextField
+        id="standard-search"
+        color="secondary"
+        fullWidth
+        onChange={handleInputChange}
+        label="Search SDNE FAQ"
+        type="search"
+        variant="outlined"
+      />
     </>
   )
 }
