78b4c4398b7a683826baa21596f37c68fc751ca5
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2021-11-14 15:29:17 -0500

    fix(homepage): correctly filter and paginate results
    This introduces the new `FAQGrid` component to the home page using the new `DefaultLayout`.
    
    This also updates the graphql query to filter out posts that do not have a question frontmatter. Additionally it introduces an excerpt  field for previewing the answer before opening.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,41 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React, { useState } from "react"
 import { graphql } from "gatsby"
-
+import { filterFAQS } from "../utils/filterFAQS"
+import DefaultLayout from "../layouts/default"
 import SearchBar from "../components/SearchBar"
+import FAQGrid from "../components/FAQGrid"
 
-const Home = ({
-  data: {
-    allMarkdownRemark: { edges },
-  },
-}) => {
-  const faqs = edges.filter(edge => !!edge.node.frontmatter.question)
+export default function Home({ data }) {
+  const { edges } = data.allMarkdownRemark
+  const [faqs] = useState(edges)
+  const [filteredFAQS, setFilteredFAQs] = useState(faqs)
+  const [, setSearchQuery] = useState("")
+
+  const handleSearchQueryUpdate = term => {
+    setFilteredFAQs(filterFAQS(faqs, term))
+    setSearchQuery(term)
+  }
+
   return (
-    <Layout>
-      <SearchBar faqs={faqs} />
-    </Layout>
+    <DefaultLayout>
+      <SearchBar onSearchQueryHandler={handleSearchQueryUpdate} />
+      <FAQGrid faqs={filteredFAQS} isPaginated={true} />
+    </DefaultLayout>
   )
 }
 
-export default Home
-
 export const pageQuery = graphql`
   query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___question] }) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___question, order: DESC }
+      filter: { frontmatter: { question: { ne: null } } }
+    ) {
       edges {
         node {
           id
           html
           rawMarkdownBody
+          excerpt(format: HTML, pruneLength: 75)
           frontmatter {
             slug
             question
